445c14def0493c533db9b99d8f7779a9c2121905
Ben Morin <bamorin00@gmail.com>
2023-09-16 19:08:57 +0000

    remove esm

--- dist/index.umd.js
+++ dist/index.umd.js
@@ -1,8 +1,7 @@
-(function (global, factory) {
-  typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
-  typeof define === 'function' && define.amd ? define(['exports'], factory) :
-  (global = typeof globalThis !== 'undefined' ? globalThis : global || self, factory(global.ATrieGrowsInJS = {}));
-})(this, (function (exports) { 'use strict';
+(function (factory) {
+  typeof define === 'function' && define.amd ? define(factory) :
+  factory();
+})((function () { 'use strict';
 
   // @ts-check
 
@@ -168,6 +167,8 @@
     this.size = 0;
   };
 
-  exports.BasicTrie = BasicTrie;
+  module.exports = {
+    BasicTrie,
+  };
 
 }));
